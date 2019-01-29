e23d51013f031e98566ae200386f867b8443a439
Martin Costa <martincosta177@gmail.com>
2019-01-29 13:06:14 -0300

    ADd retrofit and GSON dependencies. Create Retrofit Singleton

---
+++ app/src/main/java/com/arksoft/android/weathertestapp/singleton/RetrofitInstance.java
@@ -0,0 +1,27 @@
+package com.arksoft.android.weathertestapp.singleton;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
+    /**
+     * Create an instance of Retrofit object
+     * */
+    public static Retrofit getRetrofitInstance() {
+        Gson gson = new Gson();
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
